d7272a6e5853042888087eddb4534f9933ea36a7
abhig1289 <92674907+abhig1289@users.noreply.github.com>
2022-01-11 11:34:50 +0530

    conditional rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,37 +10,49 @@ class Counter extends Component {
   //   fontSize:15,
   //   fontWeight:'bold'
   // }
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no Tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
   render() {
     // let classes = "badge m-2 badge-";
     // classes += this.state.count === 0 ? "warning" : "primary";
 
     return (
       <React.Fragment>
+        {this.state.tags.length === 0 && "Please create new tag"}
+        {this.renderTags()}
         {/* <h1>Welcome to the world of PUSPAAA!!!</h1> */}
         {/* <span>{this.state.count}</span> */}
         {/* <img src={this.state.imageUrl} alt=""/> */}
         {/* <span  style={this.styles} className='badge badge-primary m-2'>{this.formatCount()}</span> */}
         {/* <span  style={{fontSize:30}} className='badge badge-primary m-2'>{this.formatCount()}</span> */}
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="badge badge-secondary m-2">Increment</button>
-        <ul>
+        {/* <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="badge badge-secondary m-2">Increment</button> */}
+        {/* <ul>
           {this.state.tags.map((tag) => (
             <li key={tag}>{tag}</li>
           ))}
-        </ul>
+        </ul> */}
       </React.Fragment>
     );
   }
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
+  // getBadgeClasses() {
+  //   let classes = "badge m-2 badge-";
+  //   classes += this.state.count === 0 ? "warning" : "primary";
+  //   return classes;
+  // }
 
-  formatCount() {
-    const { count } = this.state;
-    // return count === 0 ? <h1>Zero</h1> : count;
-    return count === 0 ? "Zero" : count;
-  }
+  // formatCount() {
+  //   const { count } = this.state;
+  //   // return count === 0 ? <h1>Zero</h1> : count;
+  //   return count === 0 ? "Zero" : count;
+  // }
 }
 export default Counter;
