9dfc5777a2f3cb60994bbac06e1155f8f4cdbfca
abhig1289 <92674907+abhig1289@users.noreply.github.com>
2022-01-11 22:09:22 +0530

    lift the State up

---
+++ src/components/App.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import Counters from "./counters";
+import NavBar from "./NavBar";
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 4 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+  handleIncrement = (counter) => {
+    // console.log(counter)
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    console.log(this.state.counters[0]);
+    this.setState({ counters });
+  };
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    // console.log("Event Handle Called", counterId);
+    this.setState({ counters });
+  };
+  render() {
+    return (
+      <div className="container">
+        <NavBar
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </div>
+    );
+  }
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,19 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import bootstrap from "bootstrap"; // eslint-disable-line no-unused-vars
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Counters from "./components/counters";
-import NavBar from "./components/NavBar";
 
-function App() {
-  return (
-    <div className="container">
-      <NavBar />
-      <main className="container">
-        <Counters />
-      </main>
-    </div>
-  );
-}
+import App from "./components/App";
+
+
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
